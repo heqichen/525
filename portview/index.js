37e6f8ca312e2089e8910dfbe0ccdef34ab3dbe4
HE Qichen <heqichen@gmail.com>
2015-06-16 12:18:51 +0800

    now can read port from arduino

--- portview/index.js
+++ portview/index.js
@@ -9,14 +9,27 @@ var serialPort = new SerialPort(portname, {
 	parser: serialport.parsers.readline("\n")
 }, false); // this is the openImmediately flag [default is true]
 
-
+var portvalue = [];
 
 serialPort.on("data", function (data) {
   try {
 		//console.log(data.toString());
 		var port = JSON.parse(data.toString()); // Produces a SyntaxError
+		if (port.message === "portview")
+		{
+			var i;
+			for (i=0; i<14; ++i)
+			{
+				portvalue[i] = undefined;
+			}
+			for (i=0; i<port.portlist.length; ++i)
+			{
+				portvalue[Number(port.portlist[i])] = port.valuelist[i];
+			}
 
-    console.log(port);
+			console.log(portvalue);
+		}
+		//console.log(port);
 	} catch (error) {
 		// Handle the error
 		//console.log(error.message);
@@ -29,7 +42,7 @@ serialPort.open()
 		setInterval(function(){
 			//console.log("a");
 			serialPort.write("p");
-		}, 1000);
+		}, 100);
 	}, 2000);
 }
 
