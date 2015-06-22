ee4f740d7f2be2a0e9d3aa423fd53b8a64b1e9f6
HE Qichen <heqichen@gmail.com>
2015-06-22 18:51:24 +0800

    restructure the directory

--- example/01-random-blink/efb/efb_status_runnable.cpp
+++
@@ -1,9 +0,0 @@
-#include "efb_status_runnable.h"
-
-EfbStatusRunnable::EfbStatusRunnable(EventCallback cb, EfbDevice *dev, int targetStatus)
-	:	EfbRunnable		(cb, EFB_THREAD_SYNC),
-		mDevice			(dev),
-		mTargetStatus	(targetStatus)
-{
-
-}
